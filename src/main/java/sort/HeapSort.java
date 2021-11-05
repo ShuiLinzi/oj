0de1fd004c73c1363ffb3132966fde8635267c99
凛 <chenguoqinga@gmail.com>
2021-11-05 21:17:33 +0800

    堆结构与堆排序

---
+++ src/main/java/sort/HeapSort.java
@@ -0,0 +1,50 @@
+package sort;
+
+public class HeapSort {//heap的堆的意思
+
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {//把数组的整体位置搞成大根堆
+            heapInsert(arr, i);//O(logN)
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    private static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    //堆化
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;//左孩子的下标
+        while (left < heapSize) {//下方还有孩子的时候
+            //两个孩子中，谁的值大，把下标给largest
+            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            //父和孩子之间，谁的直达，把下标给largest
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (largest == index) {
+                break;
+            }
+            swap(arr, largest, index);
+            index = largest;
+            left = index * 2 + 1;
+        }
+    }
+
+    //堆排序
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
