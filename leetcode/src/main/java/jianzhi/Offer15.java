9ae7f44e27c5849e951de7ab5aa849d069168582
凛 <chenguoqinga@gmail.com>
2021-12-26 23:34:57 +0800

    剑指解题思路和答案

--- leetcode/src/main/java/jianzhi/Offer15.java
+++ leetcode/src/main/java/jianzhi/Offer15.java
@@ -4,6 +4,12 @@ package jianzhi;
 //编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量)。
 //https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/
 //解题思路：进行位运算，如果与上1为1，则说明最右边的数字为1，然后进行++循环
+//">>>"无符号右移
+//操作规则：无论正负数，前面补零。
+//">>"右移
+//操作规则：正数前面补零，负数前面补1
+//"<<"左移
+//操作规则：无论正负数，后面补零。
 public class Offer15 {
     public int hammingWeight(int n) {
         int flag = 0;
@@ -13,4 +19,15 @@ public class Offer15 {
         }
         return flag;
     }
+
+    //解题方法二：
+    public int hammingWeight2(int n) {
+        int res = 0;
+        while (n != 0) {
+            n &= n - 1;
+            res++;
+        }
+        return res;
+    }
+
 }
