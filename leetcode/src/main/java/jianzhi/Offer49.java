8728bc2c8672478661b02be043765c240e60f71b
凛 <chenguoqinga@gmail.com>
2022-01-19 20:03:34 +0800

    剑指解题思路和答案

---
+++ leetcode/src/main/java/jianzhi/Offer49.java
@@ -0,0 +1,21 @@
+package jianzhi;
+
+//剑指 Offer 49. 丑数
+//我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+//https://leetcode-cn.com/problems/chou-shu-lcof/
+public class Offer49 {
+    public int nthUglyNumber(int n) {
+        int a = 0, b = 0, c = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int n2 = dp[a] * 2, n3 = dp[b] * 3, n5 = dp[c] * 5;
+            dp[i] = Math.min(Math.min(n2, n3), n5);
+            if (dp[i] == n2) a++;
+            if (dp[i] == n3) b++;
+            if (dp[i] == n5) c++;
+
+        }
+        return dp[n - 1];
+    }
+}
