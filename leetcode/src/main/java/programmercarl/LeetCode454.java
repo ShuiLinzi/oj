df7a1f91dd79024c3034c24755df6ee1985e9359
凛 <chenguoqinga@gmail.com>
2022-02-07 12:58:57 +0800

    力扣题目

--- leetcode/src/main/java/programmercarl/LeetCode454.java
+++ leetcode/src/main/java/programmercarl/LeetCode454.java
@@ -1,8 +1,33 @@
 package programmercarl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode454 {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int res = 0;
+        int temp;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            for (int m : nums2) {
+                temp = i + m;
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                } else {
+                    map.put(temp, 1);
+                }
+            }
+        }
 
+        for (int i : nums3) {
+            for (int m : nums4) {
+                temp = i + m;
+                if (map.containsKey(0 - temp)) {
+                    res += map.get(0 - temp);
+                }
+            }
+        }
+        return res;
     }
 
 }
