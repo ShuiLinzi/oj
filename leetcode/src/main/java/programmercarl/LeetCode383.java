333d69a85f12a1edde85e5e4e0522217fc1ebad4
凛 <chenguoqinga@gmail.com>
2022-02-08 22:17:02 +0800

    力扣题目

---
+++ leetcode/src/main/java/programmercarl/LeetCode383.java
@@ -0,0 +1,20 @@
+package programmercarl;
+
+public class LeetCode383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+        int temp;
+        for (int i = 0; i < magazine.length(); i++) {
+            temp = magazine.charAt(i) - 'a';
+            arr[temp]++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            temp = ransomNote.charAt(i) - 'a';
+            if (arr[temp] > 0) {
+                arr[temp]--;
+            } else return false;
+        }
+        return true;
+    }
+
+}
