639c71de5d1c5f65b5de956cfc5aaad612476bf9
凛 <chenguoqinga@gmail.com>
2022-04-23 10:52:08 +0800

    力扣题目

--- leetcode/src/main/java/programmercarl/LeetCode110.java
+++ leetcode/src/main/java/programmercarl/LeetCode110.java
@@ -1,11 +1,25 @@
 package programmercarl;
 
 public class LeetCode110 {
-    public boolean isBalanced(TreeNode root) {
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(20);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node4 = new TreeNode(7);
+        node.left = node1;
+        node.right = node2;
+        node2.left = node3;
+        node2.right = node4;
+        boolean balanced = isBalanced(node);
+        System.out.println(balanced);
+    }
+
+    public static boolean isBalanced(TreeNode root) {
         return balanced(root) != -1;
     }
 
-    private int balanced(TreeNode root) {
+    private static int balanced(TreeNode root) {
         if (root == null) {
             return 0;
         }
--- leetcode/src/main/java/programmercarl/LeetCode70.java
+++ leetcode/src/main/java/programmercarl/LeetCode70.java
@@ -15,4 +15,19 @@ public class LeetCode70 {
         }
         return sum;
     }
+
+    public int climbStairs2(int n) {
+        //以前我没得选，现在，我要用动态规划跳楼梯！！！！
+        int[] weight = new int[]{1, 2};
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int j = 0; j <= n; j++) {
+            for (int i = 0; i < weight.length; i++) {
+                if (j >= weight[i]) {
+                    dp[j] += dp[j - weight[i]];
+                }
+            }
+        }
+        return dp[n];
+    }
 }
